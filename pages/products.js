636a1c21f621b338c1a720f026083a70f1441d4f
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-13 02:20:37 +0530

    feature : get Products and display

--- pages/products.js
+++ pages/products.js
@@ -1,17 +1,56 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export default function Products(){
-    useEffect(() => {
-        axios.get('/api/products').then(res => {
-            console.log(res.data);
-        })
-    },[])
-    return (
-        <Layout>
-            <Link href={'/products/new'} className="bg-blue-900 rounded-md py-1 px-3 text-white">Add new product</Link>
-        </Layout>
-    )
+export default function Products() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("/api/products").then((res) => {
+      setProducts(res.data);
+    });
+  }, []);
+  return (
+    <Layout>
+      <Link
+        href={"/products/new"}
+        className="bg-blue-900 rounded-md py-1 px-3 text-white"
+      >
+        Add new product
+      </Link>
+      <table className="basic mt-2">
+        <thead>
+          <tr>
+            <td>Product Name</td>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product._id}>
+              <td>{product.title}</td>
+              <Link href={"/products/edit" + product._id}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                  />
+                </svg>
+                Edit
+              </Link>
+              <td>{product.description}</td>
+              <td>{product.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
 }
