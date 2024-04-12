54fc8bac8fe2b5de8b70afa88c60aab1dc40188a
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-13 01:12:44 +0530

    Add Values with useState for New Product

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,15 +1,20 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
 
 export default function NewProduct() {
+    const [title,setTitle]  = useState('');
+    const [price,setPrice]  = useState('');
+    const [description,setDescription]  = useState('');
+
     return (
         <Layout>
         <h1 className="text-blue-900 mb-2 text-xl">New Product</h1>
         <label>Product Name</label>
-           <input type="text" placeholder="product name"/>
+           <input value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder="product name"/>
            <label>Description</label>
-           <textarea placeholder="description"></textarea>
+           <textarea value={description} onChange={e => setDescription(e.target.value)} placeholder="description"></textarea>
            <label>Price in(USD)</label>
-           <input placeholder="price" type="number" />
+           <input value={price} onChange={e => setPrice(e.target.value)} placeholder="price" type="number" />
            <button className="btn-primary">Save</button>
         </Layout>
     )
