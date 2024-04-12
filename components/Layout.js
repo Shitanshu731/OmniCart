97ad4cb3c1665d62ed843ae75683304b50b22499
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-13 01:08:48 +0530

    Style New Product Section

--- components/Layout.js
+++ components/Layout.js
@@ -6,15 +6,18 @@ export default function Layout({children}) {
   return (
    <div className="bg-blue-900 w-screen h-screen flex items-center">
     <div className="text-center w-full">
-      <button className="bg-white p-4 px-6 rounded-lg" onClick={()=> signIn('google')}>Login</button>
+      <button className="bg-white p-4 px-6 rounded-lg" onClick={()=> signIn('google')}>Login with Google</button>
     </div>
    </div>
   )
 }
   return (
+    
     <div className="bg-blue-900 min-h-screen flex">
     <Nav />
         <div className="bg-white flex-grow mt-2 mr-2 rounded-lg p-4 mb-0">{children}</div>
+        
     </div>
+    
     )
 }
--- pages/products/new.js
+++ pages/products/new.js
@@ -3,7 +3,14 @@ import Layout from "@/components/Layout";
 export default function NewProduct() {
     return (
         <Layout>
-            new product
+        <h1 className="text-blue-900 mb-2 text-xl">New Product</h1>
+        <label>Product Name</label>
+           <input type="text" placeholder="product name"/>
+           <label>Description</label>
+           <textarea placeholder="description"></textarea>
+           <label>Price in(USD)</label>
+           <input placeholder="price" type="number" />
+           <button className="btn-primary">Save</button>
         </Layout>
     )
 }
