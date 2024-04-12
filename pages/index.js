179695675093aaee1355f485ebc86c32bddb2e84
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-13 00:14:40 +0530

    Add Nav Section

--- pages/index.js
+++ pages/index.js
@@ -6,21 +6,15 @@ export default function Home() {
   return (
    <div className="bg-blue-900 w-screen h-screen flex items-center">
     <div className="text-center w-full">
-      <button onClick={()=> signIn('google')}>Login</button>
+      <button className="bg-white p-4 px-6 rounded-lg" onClick={()=> signIn('google')}>Login</button>
     </div>
    </div>
   )
 }
   return (
-    <div className="text-blue-900  flex justify-between">
-      <h2>
+    <div className="text-black bg-blue-900  flex justify-between">
+    <Nav />
         Hello User {session.user.email}
-      </h2>
-      <Nav />
-      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
-        <span className="px-2">
-        </span>
-      </div>
     </div>
     )
 }
