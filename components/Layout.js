24a70a8adb6097926d8b76fc275f8a818a5e2189
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-13 00:27:28 +0530

    Add Layout page

---
+++ components/Layout.js
@@ -0,0 +1,20 @@
+import Nav from "@/components/Nav"
+import {useSession, signIn, signOut} from "next-auth/react"
+export default function Layout({children}) {
+  const {data : session} = useSession();
+  if(!session){
+  return (
+   <div className="bg-blue-900 w-screen h-screen flex items-center">
+    <div className="text-center w-full">
+      <button className="bg-white p-4 px-6 rounded-lg" onClick={()=> signIn('google')}>Login</button>
+    </div>
+   </div>
+  )
+}
+  return (
+    <div className="bg-blue-900 min-h-screen flex">
+    <Nav />
+        <div className="bg-white flex-grow mt-2 mr-2 rounded-lg p-4 mb-0">{children}</div>
+    </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
-import Nav from "@/components/Nav"
-import {useSession, signIn, signOut} from "next-auth/react"
+import Layout from "@/components/Layout";
+
 export default function Home() {
-  const {data : session} = useSession();
-  if(!session){
-  return (
-   <div className="bg-blue-900 w-screen h-screen flex items-center">
-    <div className="text-center w-full">
-      <button className="bg-white p-4 px-6 rounded-lg" onClick={()=> signIn('google')}>Login</button>
-    </div>
-   </div>
+  return(
+    <Layout>
+      Test
+    </Layout>
   )
-}
-  return (
-    <div className="bg-blue-900 min-h-screen flex">
-    <Nav />
-        <div className="bg-white flex-grow mt-2 mr-2 rounded-lg p-4 mb-0">Hello User {session.user.email}</div>
-    </div>
-    )
 }
