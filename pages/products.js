63a08568e8288b809db92cfc2efbd0a9dac2b6b0
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-13 00:55:34 +0530

    Create a new product page

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,10 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Products(){
     return (
         <Layout>
-            Products Page Here
+            <Link href={'/products/new'} className="bg-blue-900 rounded-md py-1 px-3 text-white">Add new product</Link>
         </Layout>
     )
 }
