987489e02e40038da291f687d2574e2f0a664bc8
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-13 00:44:16 +0530

    Home page complete

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  return(
+  const { data: session } = useSession();
+  return (
     <Layout>
-      Test
+      <div className="flex justify-between text-blue-900">
+        <h2>Hello, <b>{session?.user?.email}</b></h2>
+        <div className="bg-gray-300 flex text-black gap-3 p-1 items-center">
+          <img
+            src={session?.user?.image}
+            alt={session?.user?.name}
+            className="w-10 h-10 rounded-full"
+          />
+          <span className="px-2">
+          {session?.user?.name}
+          </span>
+        </div>
+      </div>
     </Layout>
-  )
+  );
 }
