f8f316914777de3911e625c5fcd979ae4d51d727
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-13 23:38:32 +0530

    Set Image label to productForm

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -33,6 +33,21 @@ export default function ProductForm({
         <form onSubmit={createProduct}>
         <label>Product Name</label>
            <input value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder="product name"/>
+           <label >Photos</label>
+           <div>
+            <label className="w-24 h-24 text-center curso flex items-center justify-center text-sm gap-1 rounded-lg bg-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 7.5h-.75A2.25 2.25 0 0 0 4.5 9.75v7.5a2.25 2.25 0 0 0 2.25 2.25h7.5a2.25 2.25 0 0 0 2.25-2.25v-7.5a2.25 2.25 0 0 0-2.25-2.25h-.75m0-3-3-3m0 0-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 0 1 2.25 2.25v7.5a2.25 2.25 0 0 1-2.25 2.25h-7.5a2.25 2.25 0 0 1-2.25-2.25v-.75" />
+          </svg>
+          <div>
+            Upload
+          </div>
+          <input type="file" className="hidden" />
+            </label>
+            {true && (
+                <div>No photos uploaded</div>
+            )}
+           </div>
            <label>Description</label>
            <textarea value={description} onChange={e => setDescription(e.target.value)} placeholder="description"></textarea>
            <label>Price in(USD)</label>
