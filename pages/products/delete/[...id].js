946f0d11a75445656ce571dfcd03a8b55a12fa94
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-13 19:18:56 +0530

    Create a delete specific Product page

---
+++ pages/products/delete/[...id].js
@@ -0,0 +1,15 @@
+import Layout from "@/components/Layout";
+
+export default function DeleteProduct() {
+  return(
+  <Layout>
+  <h1>Do you Really want to delete X</h1>
+    <button>
+        Yes
+    </button>
+    <button>
+        No
+    </button>
+  </Layout>
+  )
+}
