81f287b9f288d8c3ad5ad0af53c4d91509589c0d
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-17 13:51:04 +0530

    Create categories page

---
+++ pages/categories.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+export default function Categories(){
+    return(
+        <Layout>
+            <div>
+                Categories
+            </div>
+        </Layout>
+    )
+}
