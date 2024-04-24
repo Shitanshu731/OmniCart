249cedd838b734bb037e0561ae9c0b326af542af
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-25 00:05:49 +0530

    Build check

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '@/styles/globals.css';
+// import '@/styles/globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 export default function App({Component, pageProps: { session, ...pageProps }}) {
   return (
